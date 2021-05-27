4983504e86e0496c84a85d309ee7024c0219c39c
bingo-tingo <77322132+bingo-tingo@users.noreply.github.com>
2021-05-27 18:21:39 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,6 +1,8 @@
-var starImg, fairyImg, bgImg;
-var fairy , fairyVoice;
+var starImg,bgImg;
 var star, starBody;
+var fairy,fairyImg,fairyVoice;
+var fairyImage1,fairyImage2;
+//create variable for fairy sprite and fairyImg
 
 const Engine = Matter.Engine;
 const World = Matter.World;
@@ -10,20 +12,21 @@ const Body = Matter.Body;
 function preload()
 {
 	starImg = loadImage("images/star.png");
-	fairyImg = loadAnimation("images/fairyImage1.png","images/fairyImage2.png");
 	bgImg = loadImage("images/starNight.png");
+    fairyImg = loadAnimation("images/fairyImage1.png","images/fairyImage2.png");
+	//load animation for fairy here
 	fairyVoice = loadSound("sound/JoyMusic.mp3");
-
 }
 
 function setup() {
-	createCanvas(800, 750);
+	createCanvas(700, 650);
 
-	// fairyVoice.play();
+	//write code to play fairyVoice sound
 
-	fairy = createSprite(130, 520);
-	fairy.addAnimation("fairyflying",fairyImg);  
-	fairy.scale =0.25;
+	//create fairy sprite and add animation for fairy
+	fairy = createSprite(160,300);
+    fairy.addAnimation("fairyflying",fairyImg); 
+	fairy.scale = 0.2;
 
 	star = createSprite(650,30);
 	star.addImage(starImg);
@@ -34,44 +37,39 @@ function setup() {
 
 	starBody = Bodies.circle(650 , 30 , 5 , {restitution:0.5, isStatic:true});
 	World.add(world, starBody);
-
-	if(starBody.position.y > 470){
-
-	}
 	
 	Engine.run(engine);
-
 }
-
 
 function draw() {
   background(bgImg);
 
-  star.x = starBody.position.x;
-  star.y = starBody.position.y;
+  star.x = starBody.position.x 
+  star.y = starBody.position.y 
 
   console.log(star.y);
-  
-	if(star.y > 470 && starBody.position.y > 470 ){
-		Matter.Body.setStatic(starBody,true);
-	}
+
+  //write code to stop star in the hand of fairy
+  if(star.y > 470 && starBody.position.y > 470) {
+	Matter.Body.setStatic(starBody,true);
+  }
 
   drawSprites();
 }
 
 function keyPressed() {
-	//write code here
-	if (keyCode === LEFT_ARROW) {
+
+	if (keyCode === DOWN_ARROW) {
+		Matter.Body.setStatic(starBody,false); 
+	}
+
+	//writw code to move fairy left and right
+
+    if (keyCode === LEFT_ARROW) {
 		fairy.x = fairy.x - 10;
 	}
 
 	if (keyCode === RIGHT_ARROW) {
 		fairy.x = fairy.x + 10;
-	}
-
-	if (keyCode === DOWN_ARROW) {
-		Matter.Body.setStatic(starBody, false);
-		// star.velocityY=8
-	
 	}
 }
